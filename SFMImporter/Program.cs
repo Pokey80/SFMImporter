30041ba7babfdfd7cadb76fa0b331e09adfbb6c8
Darrin Turner <DTurner@SFM.STATE.IL.US>
2017-11-20 09:02:30 -0600

    Add project files.

---
+++ SFMImporter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
